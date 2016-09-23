ef1ffeb469ef61e30bae2f9efbd6a86b9a492a3a
Tijmen Brommet <tijmen@gmail.com>
2016-09-23 10:47:37 +0100

    Also test specialist documents
    https://github.com/alphagov/govuk-content-schemas/pull/397 makes
    testing with specialist documents possible, because this gem won't
    generate invalid data for `anyOf` schemas.

--- spec/lib/random_example_spec.rb
+++ spec/lib/random_example_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe GovukSchemas::RandomExample do
-  ITERATIONS = ENV['ITERATIONS'] || 1
-
   describe '#payload' do
     GovukSchemas::Schema.all.each do |file_path, schema|
-      # Specialist documents have complex schemas that are hard to generate content for.
-      skipped = file_path.match("specialist_document")
-
-      it "generates valid content for schema #{file_path}", skip: skipped do
-        ITERATIONS.times do
-          # This will raise an informative error if an invalid schema is generated.
-          GovukSchemas::RandomExample.new(schema: schema).payload
-        end
+      it "generates valid content for schema #{file_path}" do
+        # This will raise an informative error if an invalid schema is generated.
+        GovukSchemas::RandomExample.new(schema: schema).payload
       end
     end
   end
