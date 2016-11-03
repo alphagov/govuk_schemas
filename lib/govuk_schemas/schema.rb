eb17367e3131da9b352c412b9a44410d30a9f9ee
Tijmen Brommet <tijmen@gmail.com>
2016-11-03 17:30:58 +0000

    Fix GovukSchemas::RandomExample.for_schema
    This method stopped working because we changed the signature of
    `Schema.find`.
    
    Fixes the issue by making it use the same signature as the other method.
    
    Also adds a bunch of extra documentation and makes the new method
    private.

--- lib/govuk_schemas/schema.rb
+++ lib/govuk_schemas/schema.rb
@@ -3,37 +3,22 @@ module GovukSchemas
     # Find a schema by name
     #
     # @param schema [Hash] Type => Name of the schema/format:
-    # Accepted arguments:
-    # { links_schema: schema_name }
-    # { frontend_schema: schema_name }
-    # { publisher_schema: schema_name }
-    # { notification_schema: schema_name }
-    def self.find(schema)
-      file_path = "#{GovukSchemas::CONTENT_SCHEMA_DIR}/dist/formats/#{type_location(schema)}"
-      JSON.parse(File.read(file_path))
-    end
-
-    # Find a schema type location by name
+    # @example
     #
-    # @param schema [Hash] Type => Name of the schema/format:
-    # Accepted arguments:
-    # { links_schema: schema_name }
-    # { frontend_schema: schema_name }
-    # { publisher_schema: schema_name }
-    # { notification_schema: schema_name }
-    def self.type_location(schema)
-      type, schema_name = schema.to_a.flatten
-      {
-        links_schema: "#{schema_name}/publisher_v2/links.json",
-        frontend_schema: "#{schema_name}/frontend/schema.json",
-        publisher_schema: "#{schema_name}/publisher_v2/schema.json",
-        notification_schema: "#{schema_name}/notification/schema.json",
-      }[type]
+    #   GovukSchemas::Schema.find(links_schema: "detailed_guide")
+    #   GovukSchemas::Schema.find(frontend_schema: "detailed_guide")
+    #   GovukSchemas::Schema.find(publisher_schema: "detailed_guide")
+    #   GovukSchemas::Schema.find(notification_schema: "detailed_guide")
+    # @return [Hash] the JSON schema as a hash
+    def self.find(schema)
+      file_path = "#{GovukSchemas::CONTENT_SCHEMA_DIR}/dist/formats/#{location_for_schema_name(schema)}"
+      JSON.parse(File.read(file_path))
     end
 
     # Return all schemas in a hash, keyed by schema name
     #
     # @param schema_type [String] The type: frontend, publisher, notification or links
+    # @return [Array<Hash>] List of JSON schemas as hashes
     def self.all(schema_type: '*')
       schema_type = "publisher_v2" if schema_type == "publisher"
       Dir.glob("#{GovukSchemas::CONTENT_SCHEMA_DIR}/dist/formats/*/#{schema_type}/*.json").reduce({}) do |hash, file_path|
@@ -45,8 +30,22 @@ module GovukSchemas
     # Return a random schema of a certain type
     #
     # @param schema_type [String] The type: frontend, publisher, notification or links
+    # @return [Hash] a JSON schema as a hash
     def self.random_schema(schema_type:)
       all(schema_type: schema_type).values.sample
     end
+
+    # @private
+    def self.location_for_schema_name(schema)
+      type, schema_name = schema.to_a.flatten
+      {
+        links_schema: "#{schema_name}/publisher_v2/links.json",
+        frontend_schema: "#{schema_name}/frontend/schema.json",
+        publisher_schema: "#{schema_name}/publisher_v2/schema.json",
+        notification_schema: "#{schema_name}/notification/schema.json",
+      }[type]
+    end
+
+    private_class_method :location_for_schema_name
   end
 end
