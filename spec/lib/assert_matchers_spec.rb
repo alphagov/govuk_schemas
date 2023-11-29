e9dc3c3c7ebeecaf1810a34b53d6adff8cdb7c56
Mike Patrick <mike.patrick@digital.cabinet-office.gov.uk>
2023-11-29 17:05:48 +0000

    Use "generic" schema in place of old "placeholder"
    The "placeholder" schema was removed a while ago
    (https://github.com/alphagov/publishing-api/pull/2493) with the
    intention of specs switching to using the "generic" one

--- spec/lib/assert_matchers_spec.rb
+++ spec/lib/assert_matchers_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe GovukSchemas::AssertMatchers do
   %w[publisher links frontend notification].each do |schema_type|
     describe "#assert_valid_against_#{schema_type}_schema" do
       it "detects an valid schema" do
-        example = GovukSchemas::RandomExample.for_schema("#{schema_type}_schema": "placeholder")
-        validator = GovukSchemas::Validator.new("placeholder", schema_type, example)
+        example = GovukSchemas::RandomExample.for_schema("#{schema_type}_schema": "generic")
+        validator = GovukSchemas::Validator.new("generic", schema_type, example)
         expect(self).to receive(:assert).with(true, validator.error_message)
 
-        public_send("assert_valid_against_#{schema_type}_schema", example, "placeholder")
+        public_send("assert_valid_against_#{schema_type}_schema", example, "generic")
       end
 
       it "detects an invalid schema" do
         example = { obviously_invalid: true, second_invalid_attribute: true }
-        validator = GovukSchemas::Validator.new("placeholder", schema_type, example)
+        validator = GovukSchemas::Validator.new("generic", schema_type, example)
         expect(self).to receive(:assert).with(false, validator.error_message)
 
-        public_send("assert_valid_against_#{schema_type}_schema", example, "placeholder")
+        public_send("assert_valid_against_#{schema_type}_schema", example, "generic")
       end
     end
   end
--- spec/lib/rspec_matchers_spec.rb
+++ spec/lib/rspec_matchers_spec.rb
@@ -6,29 +6,29 @@ RSpec.describe GovukSchemas::RSpecMatchers do
 
   describe "#be_valid_against_publisher_schema" do
     it "detects an valid schema" do
-      example = GovukSchemas::RandomExample.for_schema(publisher_schema: "placeholder")
+      example = GovukSchemas::RandomExample.for_schema(publisher_schema: "generic")
 
-      expect(example).to be_valid_against_publisher_schema("placeholder")
+      expect(example).to be_valid_against_publisher_schema("generic")
     end
 
     it "detects an invalid schema" do
       example = { obviously_invalid: true }
 
-      expect(example).to_not be_valid_against_publisher_schema("placeholder")
+      expect(example).to_not be_valid_against_publisher_schema("generic")
     end
   end
 
   describe "#be_valid_against_links_schema" do
     it "detects an valid schema for links" do
-      example = GovukSchemas::RandomExample.for_schema(links_schema: "placeholder")
+      example = GovukSchemas::RandomExample.for_schema(links_schema: "generic")
 
-      expect(example).to be_valid_against_links_schema("placeholder")
+      expect(example).to be_valid_against_links_schema("generic")
     end
 
     it "detects an invalid schema for links" do
       example = { obviously_invalid: true }
 
-      expect(example).to_not be_valid_against_links_schema("placeholder")
+      expect(example).to_not be_valid_against_links_schema("generic")
     end
   end
 end
